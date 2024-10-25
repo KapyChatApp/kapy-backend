df253473e9118f2edf4f12aa0f5c2b884facb3cb
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-10-25 16:49:58 +0700

    upload avatar + background to cloudinary

---
+++ lib/actions/media.action.ts
@@ -0,0 +1,58 @@
+import cloudinary from "@/clouduonary";
+import User from "@/database/user.model";
+import { Schema } from "mongoose";
+
+export async function uploadAvatar(
+  userId: Schema.Types.ObjectId | undefined,
+  url: string,
+  publicId: string
+) {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("User not exist");
+    }
+
+    if (user.avatarPublicId) {
+      await cloudinary.uploader.destroy(user.avatarPublicId);
+      console.log("Previous avatar removed from Cloudinary");
+    }
+
+    user.avatar = url;
+    user.avatarPublicId = publicId;
+    await user.save();
+
+    return { message: "Upload avatar successfully" };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function uploadBackground(
+  userId: Schema.Types.ObjectId | undefined,
+  url: string,
+  publicId: string
+) {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("User not exist");
+    }
+
+    if (user.backgroundPublicId) {
+      await cloudinary.uploader.destroy(user.backgroundPublicId);
+      console.log("Previous background removed from Cloudinary");
+    }
+
+    user.background = url;
+    user.backgroundPublicId = publicId;
+
+    await user.save();
+
+    return { message: "Upload avatar successfully" };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
