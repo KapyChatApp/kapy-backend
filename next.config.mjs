344ce2d2e6c710f8fc434750f8e8cd127ff3ec8f
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-10-25 11:24:58 +0700

    Fix OpenApi authorization header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)', // Áp dụng cho tất cả các đường dẫn
+            headers: [
+              {
+                key: 'Access-Control-Allow-Origin',
+                value: '*', // Hoặc chỉ định miền cụ thể
+              },
+              {
+                key: 'Access-Control-Allow-Methods',
+                value: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+              },
+              {
+                key: 'Access-Control-Allow-Headers',
+                value: 'Authorization, Content-Type',
+              },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
