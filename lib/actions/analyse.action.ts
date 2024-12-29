b2100d860c7263c7e4939d97a7b78138b3894ec7
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-12-29 23:10:43 +0700

    fix countBirthDay

--- lib/actions/analyse.action.ts
+++ lib/actions/analyse.action.ts
@@ -23,42 +23,21 @@ export async function countTotalSystems() {
       await User.countDocuments({ gender: false })
     ).toString();
     const today = new Date();
-    const lt18 = (await User.countDocuments({
-      birthday: {
-        $gt: new Date(
-          today.getFullYear() - 18,
-          today.getMonth(),
-          today.getDate()
-        ),
-      },
-    })).toString();
-    const gte18lte50 = (
-      await User.countDocuments({
-        birthday: {
-          $lte: new Date(
-            today.getFullYear() - 18,
-            today.getMonth(),
-            today.getDate()
-          ),
-          $gt: new Date(
-            today.getFullYear() - 50,
-            today.getMonth(),
-            today.getDate()
-          ),
-        },
-      })
-    ).toString();
-    const gt50 = (
-      await User.countDocuments({
-        birthday: {
-          $lte: new Date(
-            today.getFullYear() - 50,
-            today.getMonth(),
-            today.getDate()
-          ),
-        },
-      })
-    ).toString();
+const date18YearsAgo = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+const date50YearsAgo = new Date(today.getFullYear() - 50, today.getMonth(), today.getDate());
+
+    // Tính từng nhóm tuổi
+    const lt18 =( await User.countDocuments({
+        birthDay: { $exists: true, $gt: date18YearsAgo }
+      })).toString();
+      
+      const gte18lte50 = (await User.countDocuments({
+        birthDay: { $exists: true, $lte: date18YearsAgo, $gt: date50YearsAgo }
+      })).toString();
+      
+      const gt50 = (await User.countDocuments({
+        birthDay: { $exists: true, $lte: date50YearsAgo }
+      })).toString();
     const result: CountAnalyseResponseDTO = {
       authHistory: {
         totalPhone: totalMobile,
