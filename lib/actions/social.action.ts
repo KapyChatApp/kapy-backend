a03c27845b83279d9991f4ab10d184d15beaaa89
HoangLam21 <hoanglamdeveloper@gmail.com>
2025-04-09 09:33:54 +0700

    fix social

--- lib/actions/social.action.ts
+++ lib/actions/social.action.ts
@@ -5,81 +5,83 @@ import User from "@/database/user.model";
 import { CommentResponseDTO } from "@/dtos/CommentDTO";
 import { FileResponseDTO } from "@/dtos/FileDTO";
 import { ShortUserResponseDTO } from "@/dtos/UserDTO";
-import { getAComment } from "./comment.action";
 import Comment from "@/database/comment.model";
 import File from "@/database/file.model";
 import { getMyBFFs } from "./mine.action";
 import { Schema } from "mongoose";
 
 export const getAllBFFSocialPost = async (
-    userId:Schema.Types.ObjectId|undefined,
+  userId: Schema.Types.ObjectId | undefined,
   page: number,
   limit: number
-) => {
-  connectToDatabase();
+): Promise<PostResponseDTO[]> => {
+  await connectToDatabase();
   const skip = (page - 1) * limit;
 
+  // 🏆 1. Lấy danh sách bạn bè (BFFs)
   const myBFFs = await getMyBFFs(userId);
+  const bffIds = myBFFs.map((bff) => bff._id.toString());
 
-  const bffIds = myBFFs.map((myBFF)=>myBFF._id);
-
-  if(userId){
-    bffIds.push(userId);
+  if (userId && !bffIds.includes(userId.toString())) {
+    bffIds.push(userId.toString());
   }
-  
-  const filter = { userId: { $in: bffIds } };
 
-  const posts = await Post.find(filter)
+  // 🏆 2. Truy vấn danh sách bài viết
+  const posts = await Post.find({ userId: { $in: bffIds } })
     .sort({ createdAt: -1 })
     .skip(skip)
     .limit(limit)
     .lean();
 
-    console.log("post found: ", posts);
+  if (!posts.length) return [];
 
-  const postResponses: PostResponseDTO[] = [];
+  // 🏆 3. Lấy tất cả userId, fileId và commentId trong một lần truy vấn
+  const userIds = Array.from(new Set(posts.map((post) => post.userId.toString())));
+  const allFileIds = Array.from(new Set(posts.flatMap((post) => post.contentIds.map((id) => id.toString()))));
+  const allCommentIds = Array.from(new Set(posts.flatMap((post) => post.comments.map((id) => id.toString()))));
+  const tagUserIds = Array.from(new Set(posts.flatMap((post) => post.tagIds.map((id) => id.toString()))));
 
-  for (const post of posts) {
-    const user = await User.findById(post.userId);
-    const fileOfPost = await File.find({_id:{$in:post.contentIds}});
-    const filesResponse: FileResponseDTO[] = [];
-    for (const file of fileOfPost) {
-      const fileResponse: FileResponseDTO = {
-        _id: file._id,
-        url: file.url,
-        fileName: file.fileName,
-        width: file.width,
-        height: file.height,
-        format: file.format,
-        bytes: file.bytes,
-        type: file.type,
-      };
-      filesResponse.push(fileResponse);
-    }
+  // 🏆 4. Truy vấn dữ liệu một lần duy nhất
+  const [users, files, comments, tagUsers] = await Promise.all([
+    User.find({ _id: { $in: userIds } }).lean(),
+    File.find({ _id: { $in: allFileIds } }).lean(),
+    Comment.find({ _id: { $in: allCommentIds } }).lean(),
+    User.find({ _id: { $in: tagUserIds } }).lean(),
+  ]);
 
-    const commentResponses: CommentResponseDTO[] = [];
+  // 🏆 5. Tạo Map để tra cứu nhanh
+  const userMap = new Map(users.map((user) => [user._id.toString(), user]));
+  const fileMap = new Map(files.map((file) => [file._id.toString(), file]));
+  const commentMap = new Map(comments.map((comment) => [comment._id.toString(), comment]));
+  const tagUserMap = new Map(tagUsers.map((user) => [user._id.toString(), user]));
 
-    const comments = await Comment.find({ _id: { $in: post.comments } });
-    for (const comment of comments) {
-      const commentResponse: CommentResponseDTO = await getAComment(
-        comment._id
-      );
-      commentResponses.push(commentResponse);
-    }
+  // 🏆 6. Xử lý dữ liệu và trả về kết quả
+  return posts.map((post) => {
+    const user = userMap.get(post.userId.toString()) || { firstName: "", lastName: "", nickName: "", avatar: "" };
+    
+    // Xử lý danh sách file
+    const filesResponse: FileResponseDTO[] = post.contentIds
+      .map((id) => fileMap.get(id.toString()))
+      .filter((file): file is FileResponseDTO => !!file);
 
-    const tags: ShortUserResponseDTO[] = [];
-    const tagUsers = await User.find({ _id: { $in: post.tagIds } });
-    for (const user of tagUsers) {
-      const tag: ShortUserResponseDTO = {
+    // Xử lý danh sách comment
+    const commentResponses: CommentResponseDTO[] = post.comments
+      .map((id) => commentMap.get(id.toString()))
+      .filter((comment): comment is CommentResponseDTO => !!comment);
+
+    // Xử lý danh sách tag
+    const tags: ShortUserResponseDTO[] = post.tagIds
+      .map((id) => tagUserMap.get(id.toString()))
+      .filter((user): user is ShortUserResponseDTO => !!user)
+      .map((user) => ({
         _id: user._id,
         firstName: user.firstName,
         lastName: user.lastName,
         nickName: user.nickName,
         avatar: user.avatar,
-      };
-      tags.push(tag);
-    }
-    const postResponse: PostResponseDTO = {
+      }));
+
+    return {
       _id: post._id,
       firstName: user.firstName,
       lastName: user.lastName,
@@ -98,8 +100,5 @@ export const getAllBFFSocialPost = async (
       musicAuthor: post.musicAuthor,
       musicImageURL: post.musicImageURL,
     };
-    postResponses.push(postResponse);
-  }
-
-  return postResponses;
+  });
 };
